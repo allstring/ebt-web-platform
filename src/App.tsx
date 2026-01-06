c014c655f55c7fd8e3362dd9cb51517d74ae5860
Dahyun Kim <tbvjekgus@gmail.com>
2026-01-06 17:06:48 +0900

    feat: refactor App component and add Layout component for structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import { Button } from "@/components/ui/button"
+import { Outlet } from 'react-router-dom'
 
 function App() {
   return (
-    <>
-      <div>
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
-        <Button>Button</Button>
-      </div>
-    </>
+    <div className="min-h-screen">
+      <Outlet />
+    </div>
   )
 }
 
