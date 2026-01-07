f5a0ee3cc91cb142cf0248b0cf29c54f946a6720
Dahyun Kim <tbvjekgus@gmail.com>
2026-01-07 16:36:28 +0900

    chore: comment out "News" route in main application file

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import ScrollToTop from './lib/ScrollToTop'
 import Home from './pages/Home'
 import About from './pages/About'
 import Contact from './pages/Contact'
-import News from './pages/News'
+// import News from './pages/News'
 import Rnd from './pages/Rnd'
 import Solution from './pages/Solution/Solution'
 import Solution_Cbrn from './pages/Solution/Solution_Cbrn'
@@ -24,7 +24,7 @@ createRoot(document.getElementById('root')!).render(
             <Route index element={<Home />} />
             <Route path="about" element={<About />} />
             <Route path="contact" element={<Contact />} />
-            <Route path="news" element={<News />} />
+            {/* <Route path="news" element={<News />} /> */}
             <Route path="rnd" element={<Rnd />} />
             <Route path="solution" element={<Solution />} />
             <Route path="solution/electronic-warfare" element={<Solution_EW />} />
