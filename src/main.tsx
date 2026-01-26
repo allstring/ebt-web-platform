c18cae6d50d72df66cbff4815cee7728293e11b7
Dahyun Kim <tbvjekgus@gmail.com>
2026-01-26 08:18:51 +0000

    feat: integrate MantineProvider for enhanced theming and styling support, wrapping the application in a new provider structure

--- src/main.tsx
+++ src/main.tsx
@@ -2,21 +2,28 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
 
+import "@/styles/global.css"
+import '@mantine/core/styles.css';
+import { MantineProvider } from '@mantine/core';
 import { LocaleProvider } from "@/lib/i18n"
 import ScrollToTop from "@/lib/ScrollToTop"
 import Layout from "./layout"
 import AppRoutes from "./routes"
 import { Analytics } from "@vercel/analytics/react"
+
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <LocaleProvider>
-      <Layout>
-        <BrowserRouter>
-          <ScrollToTop />
-          <AppRoutes />
-        </BrowserRouter>
-      </Layout>
-      <Analytics/>
-    </LocaleProvider>
+    <MantineProvider>
+      <LocaleProvider>
+        <Layout>
+          <BrowserRouter>
+            <ScrollToTop />
+            <AppRoutes />
+          </BrowserRouter>
+        </Layout>
+        <Analytics />
+      </LocaleProvider>
+    </MantineProvider>
   </StrictMode>
 )
