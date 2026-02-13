df25fd721f3f5cba9984ec71d9826e4e4e08f9ce
Dahyun Kim <tbvjekgus@gmail.com>
2026-02-13 02:41:22 +0000

    feat: update footer localization with contact details and enhance Korean address descriptions

--- src/localize/ko/footer.ts
+++ src/localize/ko/footer.ts
@@ -1,13 +1,17 @@
 export const footer = {
   companyName: "이비티(주)",
   headquarters: "본사",
-  headquartersAddress: "경기도 성남시 수정구 창업로 54, 504호",
+  headquartersAddress: "경기도 성남시 수정구 창업로 54, 504호 (판교제2테크노밸리 LH기업성장센터)",
   rndCenter: "R&D 센터",
-  rndAddress: "경기도 성남시 수정구 창업로 43, B703호",
+  rndAddress: "경기도 성남시 수정구 창업로 43, B703호 (글로벌비즈센터)",
   phone: "전화",
   fax: "팩스",
   email: "이메일",
   copyright: "© 2026–Present EBT Co., Ltd. All rights reserved.",
+  phoneNumber: "031-721-6375",
+  faxNumber: "031-721-6376",
+  emailAddress: "info@ebtech.kr",
 } as const
+
 
 export type FooterStrings = typeof footer
